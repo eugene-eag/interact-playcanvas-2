3e88337e3c20361e6d679bb3af21e8c07b3f39be
evgeny-eag <86950921+evgeny-eag@users.noreply.github.com>
2021-08-11 03:44:45 +0600

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1209981.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 53270735, 53098044, 53288150, 53290424 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/53289719/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/53289720/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/53289718/1/ammo.js', 'preload' : true},
+];
